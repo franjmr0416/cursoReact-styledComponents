2019b19c418a861e1f2c737a192c94e51b14cab0
franjmr0416 <franjmr16@gmail.com>
2022-09-30 12:11:28 -0500

    estilos extendidos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`;
+
+const Content = styled.div`
+  padding: 20px 25px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "blue" : "white")};
+  color: ${(props) => (props.primary ? "white" : "blue")};
+  padding: 10px 15px;
+  border: solid 2px grey;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+//extendiendo estilos
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <P>Hola</P>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
+      <BlockButton primary>Enviar</BlockButton>
+    </Content>
   );
-}
+};
 
 export default App;
